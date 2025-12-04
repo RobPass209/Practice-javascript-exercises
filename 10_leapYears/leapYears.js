cdf80ce398f5d983c29fcd7befb81dc0b109b504
RobPass209 <96491270+RobPass209@users.noreply.github.com>
2025-12-04 07:52:59 -0400

    Practice with AND OR and other operands

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,6 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // a year is a leap year if (Divisible by 4) AND (NOT Divisible by 100 OR Divisible by 400)
+    return (year % 4 === 0 && year % 100 !==0) || year % 400 === 0; 
 
 };
 
